be159cc2ae4b5e2b9c98ee9cdacf2af326ea3297
thitalow <thitacittalow@gmail.com>
2020-04-01 15:58:43 -0500

    thita: backend communicates with frontend validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   user: User;
+  errorMessage:string;
   
   constructor(private userService: UserService, private router:Router) { 
     this.user = new User();
@@ -23,10 +24,15 @@ export class LoginComponent implements OnInit {
   login() {
     this.userService.getUser(this.user).subscribe(
       user => {
-        sessionStorage.setItem('username', user.username)
-        sessionStorage.setItem('fname', user.fname)
-        sessionStorage.setItem('id', user.userId.toString())
-        this.router.navigate([`${"userpage"}/${user.userId}`])
+        if(user != null){
+          this.user = user;
+          sessionStorage.setItem('username', user.username)
+          sessionStorage.setItem('fname', user.fname)
+          sessionStorage.setItem('id', user.userId.toString())
+          this.router.navigate([`${"userpage"}/${user.userId}`])
+        } else {
+          this.errorMessage = "Invalid Username or Password"
+        }
       }
     );
     this.user = new User();
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user-service/user.service';
+import { ValidationService } from 'src/app/services/validation/validation.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,28 @@ import { UserService } from 'src/app/services/user-service/user.service';
 })
 export class RegisterComponent implements OnInit {
   user:User;
+  errorMessage:any;
+  successMessage:String;
 
-  constructor(private userService: UserService) {
+
+  constructor(private userService: UserService, public validationService: ValidationService) {
     this.user = new User();
+    this.errorMessage = "";
    }
 
   ngOnInit(): void {
   }
 
   register() {
-    this.userService.addUser(this.user).subscribe();
+    this.errorMessage = null;
+    this.successMessage = null;
+    this.userService.addUser(this.user).subscribe(user => this.user = user, 
+      error=>this.errorMessage = error.error.error,
+      () => {if( this.errorMessage == null){
+      this.successMessage = "User Registered!"
+      }
+    });
+
     this.user = new User();
   }
   // @Input() error: string | boolean;
